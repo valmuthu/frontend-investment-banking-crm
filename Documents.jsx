51715a7002a04705a4a4066d73c90690c8e99c83
valmuthu <115936763+valmuthu@users.noreply.github.com>
2025-07-03 18:09:07 -0400

    Delete Documents.jsx

--- Documents.jsx
+++
@@ -1,8 +0,0 @@
-export default function Documents() {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold mb-4">Documents</h1>
-      <p>Upload and manage resumes, cover letters, and other files.</p>
-    </div>
-  )
-}
