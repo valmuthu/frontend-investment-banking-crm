e0c139ce36238134bf11ef9148ebc71453c01d95
valmuthu <115936763+valmuthu@users.noreply.github.com>
2025-07-03 18:07:10 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
