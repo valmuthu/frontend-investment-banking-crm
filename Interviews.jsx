24c67d53efef6c4c511e6b5120c0b3d417dd34e2
valmuthu <115936763+valmuthu@users.noreply.github.com>
2025-07-03 18:09:18 -0400

    Delete Interviews.jsx

--- Interviews.jsx
+++
@@ -1,8 +0,0 @@
-export default function Interviews() {
-  return (
-    <div>
-      <h1 className="text-2xl font-bold mb-4">Interviews</h1>
-      <p>List of interviews with status, filters, and links to contacts.</p>
-    </div>
-  )
-}
