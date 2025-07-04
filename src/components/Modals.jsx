99d1d50f0e6b13bded369366187417bd70dc5430
valmuthu <115936763+valmuthu@users.noreply.github.com>
2025-07-04 11:32:32 -0400

    Update Modals.jsx

--- src/components/Modals.jsx
+++ src/components/Modals.jsx
@@ -1035,7 +1035,7 @@ export const InterviewHistoryModal = ({ isOpen, interview, contacts, onClose, on
       </div>
     </div>
   );
-};target.value })}
+};
                 className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
             </div>
