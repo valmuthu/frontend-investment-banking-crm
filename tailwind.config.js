00670db65195be26ac9d0888a732f63089612d4f
valmuthu <115936763+valmuthu@users.noreply.github.com>
2025-07-05 11:27:36 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: '#F0F5FF',
+          100: '#D6E4FF',
+          200: '#ADC8FF',
+          300: '#84A9FF',
+          400: '#6690FF',
+          500: '#3366FF',  // your primary-500 color
+          600: '#254EDB',
+          700: '#1939B7',
+          800: '#102693',
+          900: '#091A7A',
+        },
+      },
+    },
   },
   plugins: [],
 }
