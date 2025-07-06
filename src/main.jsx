ee01f15073de9ca4f7caaf489f564c3cb4511cab
valmuthu <115936763+valmuthu@users.noreply.github.com>
2025-07-05 21:24:27 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { AuthProvider } from './contexts/AuthContext.jsx'
+import ProtectedRoute from './components/ProtectedRoute.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <ProtectedRoute>
+        <App />
+      </ProtectedRoute>
+    </AuthProvider>
   </React.StrictMode>,
 )
